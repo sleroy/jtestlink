ce61c8a0c08ef0eeb7bb22b65fb4fe7a5ed79bd5
Sylvain Leroy <sleroy0@gmail.com>
2015-04-03 09:59:03 +0200

    [fixed]

--- corolla-ui/corolla-ui-microservice/src/main/groovy/com/tocea/corolla/CorollaGuiApplication.groovy
+++ corolla-ui/corolla-ui-microservice/src/main/groovy/com/tocea/corolla/CorollaGuiApplication.groovy
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@EnableWebSecurity
 class CorollaGuiApplication {
 
 	public static void main(final String[] args) {
