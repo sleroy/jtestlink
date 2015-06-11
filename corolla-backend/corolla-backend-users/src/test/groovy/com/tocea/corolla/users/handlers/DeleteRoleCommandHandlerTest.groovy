6454e03d2c5d6e151b8ef5350f55709877a20192
dmichel <david.michel@tocea.com>
2015-06-11 15:25:43 +0200

    fixed groovy tests ignored on gradle tasks

--- corolla-backend/corolla-backend-users/src/test/groovy/com/tocea/corolla/users/handlers/DeleteRoleCommandHandlerTest.groovy
+++ corolla-backend/corolla-backend-users/src/test/groovy/com/tocea/corolla/users/handlers/DeleteRoleCommandHandlerTest.groovy
@@ -9,9 +9,11 @@ import org.junit.Rule
 
 import spock.lang.Specification
 
+import com.google.common.collect.Lists;
 import com.tocea.corolla.test.utils.FunctionalDocRule
 import com.tocea.corolla.users.commands.DeleteRoleCommand
 import com.tocea.corolla.users.dao.IRoleDAO
+import com.tocea.corolla.users.dao.IUserDAO;
 import com.tocea.corolla.users.domain.Role
 import com.tocea.corolla.utils.functests.FunctionalTestDoc
 
@@ -26,17 +28,20 @@ class DeleteRoleCommandHandlerTest extends Specification{
 	def FunctionalDocRule rule	= new FunctionalDocRule()
 
 	def IRoleDAO roleDao = Mock(IRoleDAO)
+	
+	def IUserDAO userDao = Mock(IUserDAO)
 
 	def DeleteRoleCommandHandler	handler
 
 	def Role validRole
+	
+	def Role defaultRole
 
 
 	def setup() {
-		handler = new DeleteRoleCommandHandler(
-				roleDAO : roleDao
-				)
-		validRole = new Role(id:1, name:"admin", note:"Admin role", permissions:"",defaultRole:true )
+		handler = new DeleteRoleCommandHandler(roleDAO : roleDao, userDAO : userDao)
+		validRole = new Role(id:1, name:"admin", note:"Admin role", permissions:"",defaultRole:false)
+		defaultRole = new Role(id:2, name:"default", note:"Default Role", permissions:"", defaultRole:true)
 	}
 
 
@@ -46,8 +51,12 @@ class DeleteRoleCommandHandlerTest extends Specification{
 	 * .
 	 */
 	def "test delete role existing role"() {
+		
 		given:
 		roleDao.findOne(1) >> validRole
+		roleDao.getDefaultRole() >> defaultRole
+		userDao.findByRoleId(1) >> Lists.newArrayList()
+		
 		when:
 		final DeleteRoleCommand command = new DeleteRoleCommand(roleID : 1)
 
