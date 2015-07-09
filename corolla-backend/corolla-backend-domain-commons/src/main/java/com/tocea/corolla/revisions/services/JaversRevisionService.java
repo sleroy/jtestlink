96798c9647f65b3423cb85823f2617e33327837d
dmichel <david.michel@tocea.com>
2015-07-09 18:52:33 +0200

    [revisions] add a way to insert the name of the current user in the commit description #27

--- corolla-backend/corolla-backend-domain-commons/src/main/java/com/tocea/corolla/revisions/services/JaversRevisionService.java
+++ corolla-backend/corolla-backend-domain-commons/src/main/java/com/tocea/corolla/revisions/services/JaversRevisionService.java
@@ -1,19 +1,53 @@
 package com.tocea.corolla.revisions.services;
 
 import org.javers.core.Javers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JaversRevisionService implements IRevisionService {
 
+	private static final Logger	LOGGER = LoggerFactory.getLogger(JaversRevisionService.class);
+	
+	private static String DEFAULT_USER = "unknown";
+	
 	@Autowired
 	private Javers javers;
+	
+	/**
+	 * Retrieves the name of the current user
+	 * @return
+	 */
+	private String getUsername() {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (auth != null) {
+			
+			User user = (User) auth.getPrincipal();
+			
+			if (user != null) {
+				
+				return user.getUsername();
+			}
+			
+		}
+		
+		return DEFAULT_USER;	
+	}
 	
 	@Override
 	public void commit(Object obj) {
 		
-		javers.commit("unknown", obj);
+		String username = getUsername();
+		LOGGER.info("new commit transaction for user "+username);
+		
+		javers.commit(username, obj);
 	}
 
 }
