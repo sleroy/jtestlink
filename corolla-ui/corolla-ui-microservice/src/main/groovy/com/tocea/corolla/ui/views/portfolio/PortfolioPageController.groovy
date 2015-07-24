959374feb76aa4c129fe5c4e70642aabf27b0536
dmichel <david.michel@tocea.com>
2015-07-24 10:34:31 +0200

    [projects] add rest api to retrieve the list of all projects in the portfolio

--- corolla-ui/corolla-ui-microservice/src/main/groovy/com/tocea/corolla/ui/views/portfolio/PortfolioPageController.groovy
+++ corolla-ui/corolla-ui-microservice/src/main/groovy/com/tocea/corolla/ui/views/portfolio/PortfolioPageController.groovy
@@ -2,24 +2,32 @@ package com.tocea.corolla.ui.views.portfolio
 
 import groovy.util.logging.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tocea.corolla.products.dao.IProjectDAO;
+
 @Controller
 @Slf4j
 class PortfolioPageController {
 	
-	def static final String MANAGER_PAGE = "portfolio/manager";
-	def static final String PORTFOLIO_PAGE = "portfolio/portfolio";
+	private static final String MANAGER_PAGE = "portfolio/manager";
+	private static final String PORTFOLIO_PAGE = "portfolio/portfolio";
 
+	@Autowired
+	private IProjectDAO projectDAO;
+	
 	@RequestMapping("/ui/portfolio")
 	public ModelAndView getPortfolio() {
 		
 		def model = new ModelAndView(PORTFOLIO_PAGE)
 		model.addObject "menu", "portfolio"
+		
+		model.addObject "projects", projectDAO.findAll();
 		
 		return model
 	}
