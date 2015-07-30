b5311d01ba3f718b5f3c5d3783f542f72e0631e7
dmichel <david.michel@tocea.com>
2015-07-30 10:48:10 +0200

    [portfolio] add command to change the type of a folder node #18809 #18794

---
+++ corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/portfolio/commands/ChangePortfolioFolderNodeTypeCommand.java
@@ -0,0 +1,42 @@
+package com.tocea.corolla.portfolio.commands;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.tocea.corolla.cqrs.annotations.Command;
+
+@Command
+public class ChangePortfolioFolderNodeTypeCommand {
+
+	@NotBlank
+	private Integer nodeID;
+	
+	@NotBlank
+	private String typeID;
+	
+	public ChangePortfolioFolderNodeTypeCommand() {
+		super();
+	}
+	
+	public ChangePortfolioFolderNodeTypeCommand(Integer nodeID, String typeID) {
+		super();
+		setNodeID(nodeID);
+		setTypeID(typeID);
+	}
+
+	public Integer getNodeID() {
+		return nodeID;
+	}
+
+	public void setNodeID(Integer nodeID) {
+		this.nodeID = nodeID;
+	}
+
+	public String getTypeID() {
+		return typeID;
+	}
+
+	public void setTypeID(String typeID) {
+		this.typeID = typeID;
+	}
+	
+}
