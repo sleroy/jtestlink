4cf2be69862d9cb6dbcc5ea48257cc4dc77034a4
Sylvain Leroy <sleroy0@gmail.com>
2015-08-06 13:33:48 +0200

    [fixed] error with merging

--- corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/products/commands/CreateProjectCategoryCommand.java
+++ corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/products/commands/CreateProjectCategoryCommand.java
@@ -2,30 +2,30 @@ package com.tocea.corolla.products.commands;
 
 import javax.validation.constraints.NotNull;
 
-import com.tocea.corolla.cqrs.annotations.Command;
+import com.tocea.corolla.cqrs.annotations.CommandOptions;
 import com.tocea.corolla.products.domain.ProjectCategory;
 
-@Command
+@CommandOptions
 public class CreateProjectCategoryCommand {
-
+	
 	@NotNull
 	private ProjectCategory category;
-	
+
 	public CreateProjectCategoryCommand() {
 		super();
 	}
-	
-	public CreateProjectCategoryCommand(ProjectCategory category) {
+
+	public CreateProjectCategoryCommand(final ProjectCategory category) {
 		super();
 		setCategory(category);
 	}
-
+	
 	public ProjectCategory getCategory() {
 		return category;
 	}
-
-	public void setCategory(ProjectCategory category) {
+	
+	public void setCategory(final ProjectCategory category) {
 		this.category = category;
 	}
-	
+
 }
