0f1e39cc7c27f697772da3e9de3ec2fc9f506bc2
dmichel <david.michel@tocea.com>
2015-08-10 16:17:08 +0200

    fixed issue in CQRS module : the generated exception was lost when a command failed

--- corolla-backend/corolla-backend-cqrs/src/main/java/com/tocea/corolla/cqrs/gate/CommandExecutionException.java
+++ corolla-backend/corolla-backend-cqrs/src/main/java/com/tocea/corolla/cqrs/gate/CommandExecutionException.java
@@ -3,7 +3,7 @@ package com.tocea.corolla.cqrs.gate;
 public class CommandExecutionException extends CqrsException {
 
 	public CommandExecutionException(final Object _command, final Throwable _e) {
-		super("Command " + _command + " has failed");
+		super("Command " + _command + " has failed", _e);
 	}
 
 	public CommandExecutionException(final Throwable cause) {
