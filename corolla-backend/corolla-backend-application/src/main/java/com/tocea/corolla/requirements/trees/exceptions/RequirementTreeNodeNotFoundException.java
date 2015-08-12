87e95c9b3e1e9e1264b38fe5f1b0171a91b5c427
dmichel <david.michel@tocea.com>
2015-08-12 15:10:49 +0200

    [techdebt] fixed #18864

--- corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/requirements/trees/exceptions/RequirementTreeNodeNotFoundException.java
+++ corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/requirements/trees/exceptions/RequirementTreeNodeNotFoundException.java
@@ -1,10 +1,24 @@
+/*
+ * Corolla - A Tool to manage software requirements and test cases 
+ * Copyright (C) 2015 Tocea
+ * 
+ * This file is part of Corolla.
+ * 
+ * Corolla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, 
+ * or any later version.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Corolla.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.tocea.corolla.requirements.trees.exceptions;
 
 import com.tocea.corolla.utils.domain.CorollaDomainException;
 
 public class RequirementTreeNodeNotFoundException extends CorollaDomainException {
 
-	private static String MESSAGE = "Requirement tree node not found";
+	private static final String MESSAGE = "Requirement tree node not found";
 	
 	public RequirementTreeNodeNotFoundException() {
 		super(MESSAGE);
