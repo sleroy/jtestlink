849a6ca7fad12196c783721e3b6dcc8c29540a8a
dmichel <david.michel@tocea.com>
2015-08-13 17:09:03 +0200

    add copyright and license headers on every file in corolla-backend-domain-commons

--- corolla-backend/corolla-backend-domain-commons/src/main/java/com/tocea/corolla/revisions/domain/ICommit.java
+++ corolla-backend/corolla-backend-domain-commons/src/main/java/com/tocea/corolla/revisions/domain/ICommit.java
@@ -1,3 +1,22 @@
+/*
+ * Corolla - A Tool to manage software requirements and test cases 
+ * Copyright (C) 2015 Tocea
+ * 
+ * This file is part of Corolla.
+ * 
+ * Corolla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, 
+ * or any later version.
+ * 
+ * Corolla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Corolla.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.tocea.corolla.revisions.domain;
 
 import java.util.Date;
