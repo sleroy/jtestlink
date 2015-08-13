d9101f1491079755d0cf9804ff015d4f9ee9edad
dmichel <david.michel@tocea.com>
2015-08-13 17:04:06 +0200

    add copyright and license headers on every file in corolla-backend-application

--- corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/products/rest/ProjectStatusRestController.java
+++ corolla-backend/corolla-backend-application/src/main/java/com/tocea/corolla/products/rest/ProjectStatusRestController.java
@@ -9,6 +9,11 @@
  * the Free Software Foundation, either version 2 of the License, 
  * or any later version.
  * 
+ * Corolla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
  * along with Corolla.  If not, see <http://www.gnu.org/licenses/>.
  */
